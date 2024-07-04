d3ae49364de9db8a06a516fd4dd3b66b2fe58133
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2024-07-03 22:20:28 -0700

    Create 977. Squares of A Sorted Array.py

---
+++ Part1_Array/977. Squares of A Sorted Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l, r, i = 0, len(nums)-1, len(nums)-1
+        res = [float('inf')] * len(nums) # 需要提前定义列表，存放结果
+        while l <= r:
+            if nums[l] ** 2 < nums[r] ** 2: # 左右边界进行对比，找出最大值
+                res[i] = nums[r] ** 2
+                r -= 1 # 右指针往左移动
+            else:
+                res[i] = nums[l] ** 2
+                l += 1 # 左指针往右移动
+            i -= 1 # 存放结果的指针需要往前平移一位
+        return res
