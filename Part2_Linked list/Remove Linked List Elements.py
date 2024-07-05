a07e664d7c5a611b73a6c1553f152c02a22117ae
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2024-07-04 22:50:43 -0700

    Create Remove Linked List Elements.py

---
+++ Part2_Linked list/Remove Linked List Elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # 创建虚拟头部节点以简化删除过程
+        dummy_head = ListNode(next = head)
+        
+        # 遍历列表并删除值为val的节点
+        current = dummy_head
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return dummy_head.next
